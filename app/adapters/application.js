67ac032ae76713119bb25c4d5074930666037145
keanedawg <cfife@chicagoventure.com>
2017-05-05 11:08:07 -0500

    Added REST Adapter and JSON Serializer
    Following Along with tutorial. At halfway point: "why 2 users"

---
+++ app/adapters/application.js
@@ -0,0 +1,7 @@
+// app/adapters/application.js
+
+import RESTAdapter from 'ember-data/adapters/rest';
+
+export default RESTAdapter.extend({
+  host: 'https://api.github.com'
+});
---
+++ app/serializers/application.js
@@ -0,0 +1,12 @@
+// app/serializers/application.js
+
+import Ember from 'ember';
+import JSONSerializer from 'ember-data/serializers/json';
+
+export default JSONSerializer.extend({
+
+  keyForAttribute(key) {
+    return Ember.String.decamelize(key);
+  }
+
+});
