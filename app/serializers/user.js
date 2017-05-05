1e194fbd0c36a751005e4ac470bedaa59ee51518
keanedawg <cfife@chicagoventure.com>
2017-05-05 12:43:51 -0500

    Finished

---
+++ app/serializers/user.js
@@ -0,0 +1,14 @@
+// app/serializers/user.js
+
+import ApplicationSerializer from './application';
+
+export default ApplicationSerializer.extend({
+  primaryKey: 'login',
+
+  normalizeFindRecordResponse(store, type, payload) {
+    payload.links = {
+      repositories: payload.repos_url
+    }
+    return this._super(...arguments);
+  }
+});
